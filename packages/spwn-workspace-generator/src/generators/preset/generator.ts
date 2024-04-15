236b573dfe06e29ce15c2e02be8895d0b4c78f69
zpydee <benji@spawntech.io>
2024-04-15 13:38:01 +0200

    feat: add some workspace configuration

--- packages/spwn-workspace-generator/src/generators/preset/generator.ts
+++ packages/spwn-workspace-generator/src/generators/preset/generator.ts
@@ -1,5 +1,5 @@
 import {
-  addProjectConfiguration,
+  addDependenciesToPackageJson,
   formatFiles,
   generateFiles,
   Tree,
@@ -11,13 +11,26 @@ export async function presetGenerator(
   tree: Tree,
   options: PresetGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
+  addDependenciesToPackageJson(
+    tree,
+    {
+      next: '14.0.4',
+      react: '18.2.0',
+      'react-dom': '18.2.0',
+      'spwn-workspace-generator': '0.3.0',
+    },
+    {
+      '@nx/next': '18.2.4',
+      '@nx/react': '18.2.4',
+      '@nx/storybook': '18.2.4',
+      '@nx/web': '18.2.4',
+      '@nx/workspace': '18.2.4',
+      nx: '18.2.4',
+      storybook: '^7.5.3',
+    }
+  );
+
+  const projectRoot = `.`;
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
 }
