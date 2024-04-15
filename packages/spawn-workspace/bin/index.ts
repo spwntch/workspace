12a1fa0aac36d69cfeccc279183479f93730f13a
zpydee <benji@spawntech.io>
2024-04-15 11:35:16 +0200

    feat: add prompts

--- packages/spawn-workspace/bin/index.ts
+++ packages/spawn-workspace/bin/index.ts
@@ -1,18 +1,41 @@
 #!/usr/bin/env node
 
+import { intro, outro, text } from '@clack/prompts';
 import { createWorkspace } from 'create-nx-workspace';
 
 async function main() {
-  const name = process.argv[2]; // TODO: use libraries like yargs or enquirer to set your workspace name
+  // This assumes "workspace-generator" and "spawn-workspace" are at the same version
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const presetVersion = require('../../package.json').version;
+
+  intro(`
+  {x} Ready to spawn a new workspace?
+      Let's get you started 🚀
+      v${presetVersion}`);
+
+  let name = process.argv[2]; // TODO: use libraries like yargs or enquirer to set your workspace name
   if (!name) {
-    throw new Error('Please provide a name for the workspace');
+    name = (await text({
+      message: 'What would you like to name your new workspace?',
+      initialValue: 'spawned-workspace',
+      validate(value) {
+        if (value.trim().length === 0)
+          return 'Please provide a name for the workspace';
+      },
+    })) as string;
   }
 
-  console.log(`Creating the workspace: ${name}`);
+  const addDocs = false;
+  // const addDocs = await confirm({
+  //   message: 'Would you like to add Getting Started docs?',
+  //   initialValue: true,
+  // });
 
-  // This assumes "spwn-workspace-generator" and "spawn-workspace" are at the same version
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const presetVersion = require('../package.json').version;
+  console.log(`
+
+  💪 Creating new Spawntech workspace: ${name}
+  
+  `);
 
   // TODO: update below to customize the workspace
   const { directory } = await createWorkspace(
@@ -20,11 +43,20 @@ async function main() {
     {
       name,
       nxCloud: 'skip',
-      packageManager: 'pnpm',
+      packageManager: 'npm',
+      addDocs,
     }
   );
 
-  console.log(`Successfully created the workspace: ${directory}.`);
+  outro(`
+Your new Spawntech workspace is all set 🎉.
+Find it at ${directory}.
+
+Next steps:
+  > cd ${name}
+  > npm install
+  ${addDocs ? '> npm run docs:serve' : ''}
+  `);
 }
 
 main();
