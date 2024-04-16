4fcd089dbb7416cb04c4613c8594136b53fb7071
zpydee <benji@spawntech.io>
2024-04-16 08:24:25 +0200

    feat-spwn-workspace-generator): add @spwntch/components and a first component

--- packages/spwn-workspace-generator/src/generators/preset/generator.ts
+++ packages/spwn-workspace-generator/src/generators/preset/generator.ts
@@ -21,7 +21,7 @@ export async function presetGenerator(
   addDependenciesToPackageJson(
     tree,
     {
-      '@spwntch/shell': '^0.32.1',
+      '@spwntch/components': '^0.32.1',
       '@spwntch/tailwind': '^0.32.1',
       next: '14.0.4',
       react: '18.2.0',
