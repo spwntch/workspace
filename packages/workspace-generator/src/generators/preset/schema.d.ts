b078ed80209bc97b92aeb2750abc9fdacfaf728f
zpydee <benji@spawntech.io>
2024-04-18 11:49:22 +0200

    feat: adds a landing-page generator

--- packages/workspace-generator/src/generators/preset/schema.d.ts
+++ packages/workspace-generator/src/generators/preset/schema.d.ts
@@ -1,3 +1,6 @@
+export type Boilerplates = 'website' | 'landing-page' | 'app';
 export interface PresetGeneratorSchema {
   name: string;
+  addDocs?: boolean;
+  boilerplates?: Boilerplates[];
 }
---
+++ packages/workspace-generator/src/generators/sample-landing-page/schema.d.ts
@@ -0,0 +1,3 @@
+export interface SampleLandingPageSchema {
+  name: string;
+}
