0142d5b4c8247ae1d21448148df78fc235fe7031
zpydee <benji@spawntech.io>
2024-04-22 10:17:53 +0200

    feat: adds pathname to under-contruction

--- libs/shell/src/lib/placeholders/under-construction.tsx
+++ libs/shell/src/lib/placeholders/under-construction.tsx
@@ -1,7 +1,19 @@
 'use client';
 import { UnderConstruction as SpwnUnderConstruction } from '@spwntch/shell';
+import { H1 } from '@spwntch/components';
+
+import { usePathname } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
-export const UnderConstruction = (props: PropsWithChildren) => {
-  return <SpwnUnderConstruction {...props} />;
+export const UnderConstruction = ({
+  children,
+  ...props
+}: PropsWithChildren) => {
+  const pathname = usePathname();
+  return (
+    <SpwnUnderConstruction {...props}>
+      <H1>{pathname}</H1>
+      <div className="m-8">{children}</div>
+    </SpwnUnderConstruction>
+  );
 };
