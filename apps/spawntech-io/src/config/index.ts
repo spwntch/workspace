eaab39e73d1bbef2bd30dd741bbc0b6df977d03a
zpydee <benji@spawntech.io>
2024-04-22 09:43:27 +0200

    refactor: updates nav-items to onky use links initialy

--- apps/spawntech-io/src/config/index.ts
+++ apps/spawntech-io/src/config/index.ts
@@ -30,27 +30,27 @@ export const brand: IBrand = {
 };
 
 export const primaryNav: INavItem[] = [
+  { label: 'Our Backstory', href: '/about' },
   { label: 'Docs', href: '/docs' },
-  { label: 'Services', href: '/services' },
-  {
-    label: 'Resources',
-    children: [
-      {
-        label: 'Blog',
-        description: 'ipsum lorem and that sh*t',
-        href: '/blog',
-      },
-      {
-        label: 'Case Studies',
-        description: 'ipsum lorem and that sh*t',
-        href: '/case-studies',
-      },
-      {
-        label: 'White Papers',
-        description: 'ipsum lorem and that sh*t',
-        href: '/white-papers',
-      },
-    ],
-  },
-  { label: 'About', href: '/about' },
+  { label: 'Hire Us', href: '/services' },
+  // {
+  //   label: 'Resources',
+  //   children: [
+  //     {
+  //       label: 'Blog',
+  //       description: 'ipsum lorem and that sh*t',
+  //       href: '/blog',
+  //     },
+  //     {
+  //       label: 'Case Studies',
+  //       description: 'ipsum lorem and that sh*t',
+  //       href: '/case-studies',
+  //     },
+  //     {
+  //       label: 'White Papers',
+  //       description: 'ipsum lorem and that sh*t',
+  //       href: '/white-papers',
+  //     },
+  //   ],
+  // },
 ];
