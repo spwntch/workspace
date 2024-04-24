05c75c2b17cdd9aa88a9a19285d91ed4eb2e32e3
zpydee <benji@spawntech.io>
2024-04-24 15:25:04 +0200

    reafctor: adds more relevant content and splits out config

---
+++ apps/spawntech-io/src/config/blog.ts
@@ -0,0 +1,32 @@
+// import { IArticle } from '@/mdx';
+// import { IPageSectionContent } from '@/next-shared-ui';
+
+export const articles
+// : IArticle[]
+ = [
+  {
+    meta: {
+      slug: 'the-real-reason-you-need-an-icp',
+      categories: ['Product', 'UX'],
+      keywords: [],
+      author: {
+        avatarUrl: '/dave.png',
+        name: 'Dave Benjamin',
+      },
+    },
+    coverImage: { src: '/blog/images/the-real-reason-you-need-an-icp.webp' },
+    title: 'The Real Reason You Need an ICP',
+    subtitle: `You've heard the term, but here's the real reason that not having one is holding you back. `,
+    abstract: [
+      'The term ICP is thrown around a lot, but what does it really mean?',
+      'Here’s the real reason you need an ICP and how to get one.',
+      'You might be surprised at how bad the advice you have been getting is.',
+    ],
+  },
+];
+
+// export const blogPageContent: IPageSectionContent = {
+//   heading: 'Blog',
+//   subHeading: 'The latest from our team',
+//   body: `We're always learning and growing. Here's what we've been up to lately.`,
+// };
