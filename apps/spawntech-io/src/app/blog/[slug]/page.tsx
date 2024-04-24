5ad31a3444bbaecbbc0f09be428d9694c2475521
zpydee <benji@spawntech.io>
2024-04-24 18:49:28 +0200

    feat: adds routing structure for a blog and slugs

---
+++ apps/spawntech-io/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import { UnderConstruction } from '@/shell';
+
+const ArticlePage = () => {
+  return <UnderConstruction>I&apos;m an article</UnderConstruction>;
+};
+
+export default ArticlePage;
---
+++ apps/spawntech-io/src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+import { UnderConstruction } from '@/shell';
+
+const BlogPage = () => {
+  return <UnderConstruction>I&apos;m a blog</UnderConstruction>;
+};
+
+export default BlogPage;
