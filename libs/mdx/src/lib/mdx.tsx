a9eda6f92b441536c9b2e5a993672d4857439048
zpydee <benji@spawntech.io>
2024-04-24 13:32:43 +0200

    feat: adds a boilerplate md lib

---
+++ libs/mdx/src/lib/mdx.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface MdxProps {}
+
+export function Mdx(props: MdxProps) {
+  return (
+    <div>
+      <h1>Welcome to Mdx!</h1>
+    </div>
+  );
+}
+
+export default Mdx;
