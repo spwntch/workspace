6fa0276a9a8f2d5d95de8955266b1952a29f0afe
zpydee <benji@spawntech.io>
2024-05-19 13:40:38 +0200

    feat: adds components-catalogue app

---
+++ apps/components-catalogue/app/page.tsx
@@ -0,0 +1,10 @@
+import { Components } from '../components/components';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return <Components />;
+}
---
+++ apps/components-catalogue/components/components.tsx
@@ -0,0 +1,58 @@
+'use client';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Badge,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@spwntch/ui';
+
+export const Components = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 p-3 border-2 w-fit mx-auto rounded-xl">
+      <Button className="w-fit">HomePage</Button>
+      <Button className="w-fit" variant="outline">
+        HomePage
+      </Button>
+      <Accordion type="single" collapsible className="w-96">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <Badge>look at me!!!</Badge>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      {/* <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu> */}
+    </div>
+  );
+};
